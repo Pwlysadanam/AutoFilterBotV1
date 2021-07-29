f2a84d019b8753c1233c08a30cf305072173099e
Pwlysadanam <88115138+Pwlysadanam@users.noreply.github.com>
2021-07-29 17:47:11 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} \n @allmoviekeralatvm",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔹 Our Group 🔹', url="https://t.me/joinchat/JyPMCYXDHlk1NjZl"
                                 )
                         ]
                     ]
